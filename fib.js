d7594e050cb27578f1cf585339005c8f269706b3
jenlij <jljohns1216@gmail.com>
2017-09-20 12:45:25 -0400

    implements fibonacci series via recursion in js

---
+++ fib.js
@@ -0,0 +1,10 @@
+//first n fibonacci numbers
+function fibonacci(x, y, n) {
+    if (n === 0) {
+        return
+    }
+    console.log(x);
+    fibonacci(y, x + y, n - 1);
+}
+
+fibonacci(1, 1, 12)
